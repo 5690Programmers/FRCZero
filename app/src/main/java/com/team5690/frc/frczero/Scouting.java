7dedcd4afffa091211abab42b34f170b1beca6ff
Suzbzero5690 <5690Programmers@gmail.com>
2017-07-05 14:02:04 -0500

    comments

--- app/src/main/java/com/team5690/frc/frczero/Scouting.java
+++ app/src/main/java/com/team5690/frc/frczero/Scouting.java
@@ -20,11 +20,15 @@ import java.io.OutputStreamWriter;
 
 public class Scouting extends AppCompatActivity {
 
+
+    /* When the onclick method of BackButton is called it opens the Home.class*/
     public void BackButton(View view){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
 
+
+    /*These are our private variables that we declared, telling the app that hey, these things exist*/
     Button SaveMatch;
     private RadioGroup AutoYN;
     private RadioButton AutoButton;
@@ -46,7 +50,7 @@ public class Scouting extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scouting);
 
-
+        /*Here we tell the app that hey this is what this is and start the counters at 0 pls*/
         GearCounter = 0;
         vComments = (EditText) findViewById(R.id.vComments);
         vTeamNumber = (EditText) findViewById(R.id.vTeamNumber);
@@ -62,6 +66,10 @@ public class Scouting extends AppCompatActivity {
         vFuel = (TextView) findViewById(R.id.vFuel);
         tFuel = (TextView) findViewById(R.id.tFuel);
 
+
+        /*These things are onclicks that listen for onclicks or presses. In this case we set it up so
+        * if the pGears(Plus gears for short :P) is pressed it increases the GearCounter by one. Same thing goes
+        * for mGears(Minus gears) but it takes one away*/
         pGears.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,36 +102,44 @@ public class Scouting extends AppCompatActivity {
             }
         });
 
+        /*We got a button with the id of Save*/
         SaveMatch = (Button) findViewById(R.id.Save);
+        /*You wanna watch this button and see if people click it pal?*/
         SaveMatch.setOnClickListener(new OnClickListener() {
 
 
 
             @Override
+            /*Hey that SavedMatch button was pressed, what now?*/
             public void onClick(View v) {
 
 
-
+                /*We got selectors and multiple things that could be pressed. we wanna tell the app that
+                * a certain one was pressed. We tell it there is a group of buttons (RadioGroups)*/
                 AutoYN = (RadioGroup) findViewById(R.id.AutoGroup);
                 ClimbGroup = (RadioGroup) findViewById(R.id.ClimbGroup);
                 SaveMatch = (Button) findViewById(R.id.Save);
 
-                // get selected radio button from radioGroup
+                /*get selected radio button from radioGroup*/
                 int selectedId = AutoYN.getCheckedRadioButtonId();
                 int selectedId2 = ClimbGroup.getCheckedRadioButtonId();
 
-                // find the radiobutton by returned id
+                /* find the radiobutton by returned id*/
                 AutoButton = (RadioButton) findViewById(selectedId);
                 ClimbButton = (RadioButton) findViewById(selectedId2);
 
 
-
+                /*This is the tool we use to output string data to "myFile" we added a line separator to separate
+                 * every entry with an enter. This allows the data to be put on a separate row in a Spreadsheet  */
                 try {
                     FileOutputStream fos = new FileOutputStream(myExternalFile, true);
                     OutputStreamWriter osw = new OutputStreamWriter(fos);
                     String separator = System.getProperty("line.separator");
 
                     /* User input */
+
+                    /*Here we say fos(FileOutputStream) write this data and put it into a string so we can read it and add a , after it
+                    * Adding a , after it puts each entry into its own cell in a spreadsheet(Also know as CSV or comma separated values*/
                     fos.write((vMatchNumber.getText().toString() + ",").getBytes());
                     fos.write((vTeamNumber.getText().toString() + ",").getBytes());
                     fos.write((vGears.getText().toString() + ",").getBytes());
@@ -131,28 +147,33 @@ public class Scouting extends AppCompatActivity {
                     fos.write((AutoButton.getText().toString() + ",").getBytes());
                     fos.write((ClimbButton.getText().toString() + ",").getBytes());
                     fos.write(vComments.getText().toString().getBytes());
+                    /*All done pls close this I has nothing left for you to write*/
                     osw.append("");
                     osw.append(separator);
                     osw.flush();
                     osw.close();
                     fos.close();
+                    /*Weird catch thing I dont understand but its needed*/
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                /*Cheers, make a toast notification to tell the user your "Scouting Data Saved" rejoice
+                * as you get ready to scout for the next 4 hours, YAY!*/
                 Toast.makeText(Scouting.this, ("Scouting Data Saved"),
                         Toast.LENGTH_LONG).show();
 
             }
 
         });
-
+        /*This here checks to see if the External storage is available and if its read only, if so then it sets it to false
+        * But if its there and not read only it saves the file with the filepath and filename*/
         if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
             SaveMatch.setEnabled(false);
         } else {
            myExternalFile = new File(getExternalFilesDir(filepath), filename);
         }
     }
-
+    /*These two find out if the External Storage is Available and if its ReadOnly*/
     private static boolean isExternalStorageReadOnly() {
         String extStorageState = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
--- app/src/main/java/com/team5690/frc/frczero/pit_scouting.java
+++ app/src/main/java/com/team5690/frc/frczero/pit_scouting.java
@@ -19,11 +19,13 @@ import java.io.OutputStreamWriter;
 public class pit_scouting extends AppCompatActivity {
 
 
+    /* When the onclick method of BackButton is called it opens the Home.class*/
     public void BackButton(View view){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
 
+    /*These are our private variables that we declared, telling the app that hey, these things exist*/
     private  RadioGroup DriveGroup;
     private  RadioButton DriveButtons;
 
@@ -42,58 +44,86 @@ public class pit_scouting extends AppCompatActivity {
     private String filepath = "ScoutingData";
     File myFile;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pit_scouting);
 
+
+        /* we are defining what some of these things mean, like vPitComments being and EditText field with the id of vPitComments*/
         vPitComments = (EditText) findViewById(R.id.vPitComments);
         vPitTeamNumber = (EditText) findViewById(R.id.vPitTeamNumber);
         SavePit = (Button) findViewById(R.id.SavePit);
+       /* Here we made the Button SavePit and set an OnClickListener so it listens for a onclick or a button press*/
         SavePit.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
+            /*This calls the onclick we just set with the OnClickListener. Note you can set onclicks in two ways. 1. the way above
+            * 2. Going to the .xml and adding android:onclick="Whateveryouwant" and putting that in place of the OnClick in "public void OnClick"*/
             public void onClick(View v) {
 
+                /*This is for the booleans we have. since we have multiple selections for drivetrians and etc,
+                we want the one that is selected to be printed into our file we save*/
+
+                /*First we state private variable we initialized at the top, what it is and its ID*/
                 DriveGroup = (RadioGroup) findViewById(R.id.DriveGroup);
                 GearGroup = (RadioGroup) findViewById(R.id.GearGroup);
                 FuelGroup = (RadioGroup) findViewById(R.id.FuelGroup);
                 ClimberGroup = (RadioGroup) findViewById(R.id.ClimberGroup);
+
+                /*Then we int a thing to call it, like selectedId(it can be whatever you wanna call it just make it the same throughout for simplicity
+                * We set the certain selectedId to the name of a RadioGroup stated above and we ask for the CheckedRadioButton*/
 
                 int selectedId = DriveGroup.getCheckedRadioButtonId();
                 int selectedId2 = GearGroup.getCheckedRadioButtonId();
                 int selectedId3 = FuelGroup.getCheckedRadioButtonId();
                 int selectedId4 = ClimberGroup.getCheckedRadioButtonId();
 
+                /*Once we know what button is check we wanna capture it and set it equal to something
+                * We take the private variables we initialized at the top, tell it that its a RadioButton, then tell it that it
+                * has the selectedId of what was selected before*/
                 DriveButtons = (RadioButton) findViewById(selectedId);
                 GearButtons = (RadioButton) findViewById(selectedId2);
                 FuelButtons = (RadioButton) findViewById(selectedId3);
                 ClimberButtons = (RadioButton) findViewById(selectedId4);
 
-
+                /*This includes all the saving of the inputs*/
                 try{
+                    /*This is the tool we use to output string data to "myFile" we added a line separator to separate
+                    * every entry with an enter. This allows the data to be put on a separate row in a Spreadsheet  */
                     FileOutputStream fos = new FileOutputStream(myFile, true);
                     OutputStreamWriter osw = new OutputStreamWriter(fos);
                     String separator = System.getProperty("line.separator");
 
+                    /*Here we say fos(FileOutputStream) write this data and put it into a string so we can read it and add a , after it
+                    * Adding a , after it puts each entry into its own cell in a spreadsheet(Also know as CSV or comma separated values*/
                     fos.write((vPitTeamNumber.getText().toString() + ",").getBytes());
                     fos.write((DriveButtons.getText().toString() + ",").getBytes());
                     fos.write((GearButtons.getText().toString() + ",").getBytes());
                     fos.write((FuelButtons.getText().toString() + ",").getBytes());
                     fos.write((ClimberButtons.getText().toString() + ",").getBytes());
                     fos.write(vPitComments.getText().toString().getBytes());
+                    /*We tell the app, all done close up shop pls*/
                     osw.append("");
                     osw.append(separator);
                     osw.flush();
                     osw.close();
                     fos.close();
-
+                    /*I dont know what catches are so not much to say but I think its needed....*/
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                /*Once we successfully save the file and there wasnt some weird error that caused an IOException catch thingy
+                * the app makes a little toast notification saying "Pit Scouting Data Saved" */
                 Toast.makeText(pit_scouting.this, ("Pit Scouting Data Saved"), Toast.LENGTH_LONG).show();
             }
         });
 
+        /*This here checks to see if the External storage is available and if its read only, if so then it sets it to false
+        * But if its there and not read only it saves the file with the filepath and filename*/
         if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
             SavePit.setEnabled(false);
         } else {
@@ -102,7 +132,7 @@ public class pit_scouting extends AppCompatActivity {
 
 
     }
-
+    /*These two find out if the External Storage is Available and if its ReadOnly*/
     private static boolean isExternalStorageReadOnly() {
         String extStorageState = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {

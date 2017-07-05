58bfea208d295b6f3482e45d82cbe58187834dee
Suzbzero5690 <5690Programmers@gmail.com>
2017-07-05 14:01:56 -0500

    added comments for understanding

--- app/src/main/java/com/team5690/frc/frczero/Home.java
+++ app/src/main/java/com/team5690/frc/frczero/Home.java
@@ -13,10 +13,13 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
     }
+   /* When the onclick method of gotoScouting is called it opens the Scouting.class*/
+
     public void gotoScouting(View view){
         Intent intent = new Intent(this, Scouting.class);
         startActivity(intent);
     }
+    /*When the onclick method of gotoPitScouting is called it opens the pit_scouting.class*/
     public void gotoPitScouting(View view){
         Intent intent = new Intent(this, pit_scouting.class);
         startActivity(intent);
